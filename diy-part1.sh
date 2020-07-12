d44f6e3ca940cd996946c9721072e951bfbe869b
jxjhheric <jxjhheric@gmail.com>
2020-07-12 12:29:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git smartdns https://github.com/pymumu/smartdns' feeds.conf.default
